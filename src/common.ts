e0599058d183b31999d16bae94e6e0fc49c1a9c3
kezike <kezike13@gmail.com>
2022-01-05 18:02:17 -0500

    using dcc context

--- src/common.ts
+++ src/common.ts
@@ -1,12 +1,11 @@
 
 import { contexts as ldContexts, documentLoaderFactory } from '@transmute/jsonld-document-loader';
-import DccContextV1 from "./contexts/dcc-v1.json";
+import { CONTEXT_URL_V1, CONTEXT_V1 } from 'dcc-context';
 import { DIDDocument } from "./types";
 import { Ed25519VerificationKey2020 } from '@digitalcredentials/ed25519-verification-key-2020';
 
 const didContext = require('@digitalcredentials/did-context');
 const ed25519 = require('ed25519-signature-2020-context');
-const DccContextV1Url = "https://w3id.org/dcc/v1";
 const x25519Ctx = require('@digitalcredentials/x25519-key-agreement-2020-context');
 
 export function getController(fullDid: string) {
@@ -24,7 +23,7 @@ export function getCustomLoader() : any {
   })
   .addContext({ [ed25519.constants.CONTEXT_URL]: ed25519.contexts.get(ed25519.constants.CONTEXT_URL) })
   .addContext({ [didContext.constants.DID_CONTEXT_URL]: didContext.contexts.get(didContext.constants.DID_CONTEXT_URL) })
-  .addContext({ [DccContextV1Url]: DccContextV1 })
+  .addContext({ [CONTEXT_URL_V1]: CONTEXT_V1 })
   .addContext({ [x25519Ctx.constants.CONTEXT_URL]: x25519Ctx.contexts.get(x25519Ctx.constants.CONTEXT_URL) });
   return customLoaderProto;
 }
