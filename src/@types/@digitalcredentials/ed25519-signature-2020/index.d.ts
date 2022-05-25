2767542a6d6466b6e645d5ac10ba4823f860fef7
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-05-25 09:31:31 -0400

    Fix formatting.

--- src/@types/@digitalcredentials/ed25519-signature-2020/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-
-declare module '@digitalcredentials/ed25519-signature-2020' {
-  export class Ed25519Signature2020 {
-    constructor(options);
-  }
-}
---
+++ src/@types/index.d.ts
@@ -0,0 +1,5 @@
+declare module '@digitalcredentials/did-method-key';
+declare module '@digitalcredentials/did-web-resolver';
+declare module '@digitalcredentials/ed25519-verification-key-2020';
+declare module '@digitalcredentials/ed25519-signature-2020';
+declare module '@digitalcredentials/x25519-key-agreement-key-2020';
