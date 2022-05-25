5f78197393767e34c4610120090cb19e48e95775
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-05-25 09:18:14 -0400

    Fix lint errors.

--- src/@types/@digitalcredentials/ed25519-signature-2020/index.d.ts
+++ src/@types/@digitalcredentials/ed25519-signature-2020/index.d.ts
@@ -1,10 +1,7 @@
 /// <reference types="node" />
 
 declare module '@digitalcredentials/ed25519-signature-2020' {
-
   export class Ed25519Signature2020 {
-    constructor(options = {});
-  };
-
-
+    constructor(options);
+  }
 }
