5260d9770ba828f7c9f8d73aaae1c13dbf550261
kezike <kezike13@gmail.com>
2022-07-21 08:28:36 -0400

    updated to esm imports; properly declared vc-status-list; minor style updates

--- src/common.spec.ts
+++ src/common.spec.ts
@@ -5,13 +5,12 @@ import { getController } from './common';
 
 const fragment = 'z6MkhVTX9BF3NGYX6cc7jWpbNnR7cAjH8LUffabZP8Qu4ysC'
 const controller = `did:key:${fragment}`;
-const identifer = `${controller}#${fragment}`;
+const identifier = `${controller}#${fragment}`;
 
 describe('Common test',
   () => {
     it('should parse controller', () => {
-      const result = getController(identifer);
+      const result = getController(identifier);
       expect(result).to.equal(controller);
     });
-
   });
